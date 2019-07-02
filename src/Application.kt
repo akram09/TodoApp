c6d6d94adf267c387cbeeacc0e222bd27ad9cdf6
akram09 <a.boutouchent@esi-sba.dz>
2019-07-02 21:25:30 +0100

    initialization of the preoject

--- src/Application.kt
+++ src/Application.kt
@@ -1,10 +1,16 @@
 package com.kero.akram.todo
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
+import io.ktor.auth.Authentication
+import io.ktor.auth.basic
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.JacksonConverter
+import io.ktor.jackson.jackson
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -12,14 +18,20 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation){
-
-     }
-    routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        jackson{
+            configure(SerializationFeature.INDENT_OUTPUT, true)
         }
+     }
+    install(Authentication){
 
     }
+    routing {
+        get("/") {
+            call.respond(Person("hello" , "hey "))
+        }
 
+
+    }
 }
+data class Person(val name:String , val prenam:String)
 
