233569e02ad83a6e47e0f6e320ad46e8b4ab36b2
akram09 <a.boutouchent@esi-sba.dz>
2020-01-07 21:14:07 +0100

    update dependencies versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,16 +1,16 @@
 object Versions {
     object Kotlin {
-        const val kotlinVersion = "1.3.41"
-        const val SerializationVersion = "0.11.1"
+        const val kotlinVersion = "1.3.60"
+        const val SerializationVersion = "0.14"
         const val KoinVersion = "2.0.1"
-        const val coroutine_version = "1.3.0-RC"
+        const val coroutine_version = "1.3.3"
         const val rxJava="2.2.4"
         const val rxKotlin = "2.3.0"
         const val junit = "5.5.1"
         const val mockito = "2.13.0"
     }
     object Ktor {
-        const val ktor_version = "1.2.2"
+        const val ktor_version = "1.3.0-rc2"
         const val logback_version = "1.2.1"
         const val exposed = "0.16.1"
         const val hickariCp = "2.7.8"
